58ba33e61c408ced86bd329acfb7ddd5e8ce84e9
Justus <justuswol@hotmail.com>
2019-12-12 13:52:24 +0100

    second commit: entity update

--- src/main/java/com/KarteMeister/KMBackEnd/domein/Attraction.java
+++ src/main/java/com/KarteMeister/KMBackEnd/domein/Attraction.java
@@ -10,23 +10,39 @@ public class Attraction {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
-	private String message;
-	
-	public String getMessage() {
-		return message;
-	}
-
-	public void setMessage(String message) {
-		this.message = message;
-	}
-
+	private String location;
+	private String dateAndTime;
+	private String description;
+	private String eventName;
 	public long getId() {
 		return id;
 	}
-
 	public void setId(long id) {
 		this.id = id;
 	}
-	
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getDateAndTime() {
+		return dateAndTime;
+	}
+	public void setDateAndTime(String dateAndTime) {
+		this.dateAndTime = dateAndTime;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getEventName() {
+		return eventName;
+	}
+	public void setEventName(String eventName) {
+		this.eventName = eventName;
+	}
 	
 }
