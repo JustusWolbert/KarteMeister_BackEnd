7bdf5866df2172f940603f9c2f0bbc6e20c6c88d
Justus <justuswol@hotmail.com>
2019-12-13 13:34:07 +0100

    GET and POST request support added

--- src/main/java/com/KarteMeister/KMBackEnd/controller/AttractionRepository.java
+++ src/main/java/com/KarteMeister/KMBackEnd/controller/AttractionRepository.java
@@ -1,11 +1,18 @@
 package com.KarteMeister.KMBackEnd.controller;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.KarteMeister.KMBackEnd.domein.Attraction;
 
 @Component
 public interface AttractionRepository extends CrudRepository<Attraction, Long>{
+	
+	@Override
+	@Transactional(timeout=10)
+	public List<Attraction> findAll();
 
 }
