6d48aa9e95e7d7a26140441c9c533bd5bc6e9531
Justus <justuswol@hotmail.com>
2019-12-16 15:12:40 +0100

    GET POST PUT DELETE all included

--- src/main/java/com/KarteMeister/KMBackEnd/controller/AttractionService.java
+++ src/main/java/com/KarteMeister/KMBackEnd/controller/AttractionService.java
@@ -15,15 +15,40 @@ public class AttractionService {
 		ar.save(attr);
 	}
 	
-	@Transactional						// Transactional is used for adaptations, not needed for GET
 	public Attraction getAttractionEntry(String eventName) {
-				
+		/*		
 		for(Attraction attr : ar.findAll()) {
 			if(attr.getEventName().equals(eventName)) {
 				return attr;
 			}
 		}
 		return null;
+		*/
+		Attraction attr = ar.findByEventName(eventName);
+		return attr;
 	}
+	
+	/*
+	@Transactional						
+	public Attraction changeAttractionEntry(String eventName, String newDateAndTime) {
+		Attraction att = getAttractionEntry(eventName);
+		att.setDateAndTime(newDateAndTime);
+		return null;
+	} */
+	
+	public void setLocationForEvent(String location, String eventName) {
+		Attraction attr = ar.findByEventName(eventName);
+		attr.setLocation(location);
+		ar.save(attr);
+		
+		//return 0;
+	}
+	
+	public void delete(String eventName) {
+		Attraction attr = ar.findByEventName(eventName);
+		
+		ar.deleteById(attr.getId());
+	}
+	
 
 }
